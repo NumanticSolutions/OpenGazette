972a0b56fed28692deb1cd85618a0d6303ba34f0
Nathan Moroney <nathan.moroney@numanticsolutions.com>
2024-08-02 17:42:54 -0700

    First test - using subset zip data

--- opengazette/example_01.py
+++ opengazette/example_01.py
@@ -6,3 +6,12 @@ import zipped_patent_gazette
 zpg = zipped_patent_gazette.ZippedPatentGazette()
 
 print('example 1 : zipped patent gazette')
+
+path, name = '../data/',  'e-OG20240730_1524-5-subset-101_111.zip'
+
+archive = zpg.open_archive(path, name)
+htmls, gifs = zpg.quick_list(archive)
+
+for n in range(len(htmls)):
+  print(str(n) + " : " + htmls[n] + " - " + gifs[n])
+
